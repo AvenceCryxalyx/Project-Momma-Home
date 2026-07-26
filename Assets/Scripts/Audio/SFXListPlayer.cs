54f812def6594e95778ff50707d65db8c1b86be9
AvenceCryxalyx <avencecryxalyx@gmail.com>
2026-07-26 17:22:14 +0800

    Audio Updates, added a variety of sfx.

---
+++ Assets/Scripts/Audio/SFXListPlayer.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SFXListPlayer : MonoBehaviour
+{
+    [SerializeField] private List<AudioClip> clipList;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.loop = false;
+    }
+
+    public void Play(int index = -1)
+    {
+        if (audioSource == null)
+            return;
+
+        if (index < 0)
+            index = Random.Range(0, clipList.Count - 1);
+        audioSource.clip = clipList[index];
+        audioSource.Play();
+    }
+
+    public void Stop()
+    {
+        if (audioSource == null)
+            return;
+
+        audioSource.Stop();
+    }
+}
