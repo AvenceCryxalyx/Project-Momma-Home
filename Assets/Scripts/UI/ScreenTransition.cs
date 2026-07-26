e040a7ca3e7426cae3cc616ef8012c4627cd1038
Bert <bert.no-domain@pm.me>
2026-07-26 21:57:41 +0800

    Wipe transition overlay

---
+++ Assets/Scripts/UI/ScreenTransition.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class ScreenTransition : MonoBehaviour
+{
+    public static ScreenTransition Instance;
+    [SerializeField]
+    private Animator animator;
+
+    int wipeIdleIn = Animator.StringToHash("WipeIdleIn");
+    int RemoveOverlay = Animator.StringToHash("RemoveOverlay");
+    int StartWipeIn = Animator.StringToHash("StartWipeIn");
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+        Instance ??= this;
+        if (Instance != this)
+        {
+            Destroy(this);
+        }
+    }
+
+    public void WipeIn()
+    {
+        animator.Play(wipeIdleIn);
+        animator.SetTrigger(StartWipeIn);
+    }
+
+    public void WipeOut()
+    {
+        animator.SetTrigger(RemoveOverlay);
+    }
+}
